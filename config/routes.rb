198310608bd37a70c615e88bf8dc360b4c8bb634
safar ali <miqra019@gmail.com>
2022-10-11 16:41:37 +0500

    Complete sytling the first page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'user#index'
+  root 'static_pages#index'
 
   resources :users, only: [:index, :show]
 end
