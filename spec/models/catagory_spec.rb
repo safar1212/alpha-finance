e10216dac53a0a7ed5e347049c3a75ee4472e406
safar ali <miqra019@gmail.com>
2022-10-14 17:47:48 +0500

    Solve linters

--- spec/models/catagory_spec.rb
+++ spec/models/catagory_spec.rb
@@ -3,25 +3,23 @@ require 'rails_helper'
 RSpec.describe Catagory, type: :model do
   catagorier = Catagory.new(name: 'suger', icon: 'icon', user_id: 1)
 
-  describe 'validations' do
-    before { catagorier.save }
-    it 'is valid with valid attributes' do
-      expect(catagorier).to_not be_valid
-    end
+  before { catagorier.save }
+  it 'is valid with valid attributes' do
+    expect(catagorier).to_not be_valid
+  end
 
-    it 'is not valid without an user_id' do
-      catagorier.user_id = nil
-      expect(catagorier).to_not be_valid
-    end
+  it 'is not valid without an user_id' do
+    catagorier.user_id = nil
+    expect(catagorier).to_not be_valid
+  end
 
-    it 'is not valid without a name' do
-      catagorier.name = nil
-      expect(catagorier).to_not be_valid
-    end
+  it 'is not valid without a name' do
+    catagorier.name = nil
+    expect(catagorier).to_not be_valid
+  end
 
-    it 'is not valid without a amount' do
-      catagorier.icon = nil
-      expect(catagorier).to_not be_valid
-    end
+  it 'is not valid without a amount' do
+    catagorier.icon = nil
+    expect(catagorier).to_not be_valid
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,30 +3,28 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   itemer = Item.new(name: 'suger', amount: '4', user_id: 1, catagory_id: 2)
 
-  describe 'validations' do
-    before { itemer.save }
-    it 'is valid with valid attributes' do
-      expect(itemer).to_not be_valid
-    end
+  before { itemer.save }
+  it 'is valid with valid attributes' do
+    expect(itemer).to_not be_valid
+  end
 
-    it 'is not valid without an user_id' do
-      itemer.user_id = nil
-      expect(itemer).to_not be_valid
-    end
+  it 'is not valid without an user_id' do
+    itemer.user_id = nil
+    expect(itemer).to_not be_valid
+  end
 
-    it 'is not valid without a catagory_id' do
-      itemer.catagory_id = nil
-      expect(itemer).to_not be_valid
-    end
+  it 'is not valid without a catagory_id' do
+    itemer.catagory_id = nil
+    expect(itemer).to_not be_valid
+  end
 
-    it 'is not valid without a name' do
-      itemer.name = nil
-      expect(itemer).to_not be_valid
-    end
+  it 'is not valid without a name' do
+    itemer.name = nil
+    expect(itemer).to_not be_valid
+  end
 
-    it 'is not valid without a amount' do
-      itemer.amount = nil
-      expect(itemer).to_not be_valid
-    end
+  it 'is not valid without a amount' do
+    itemer.amount = nil
+    expect(itemer).to_not be_valid
   end
 end
