5870f21cfd234abf203ea702e2bbe4c8844c1156
safar ali <miqra019@gmail.com>
2022-10-14 15:46:43 +0500

    Complete important testing

---
+++ spec/models/catagory_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Catagory, type: :model do
+  Catagorier = Catagory.new(name: "suger", icon: "icon", user_id: 1)
+
+  before { Catagorier.save }
+  it 'is valid with valid attributes' do
+    expect(Catagorier).to_not be_valid
+  end
+
+  it 'is not valid without an user_id' do
+    Catagorier.user_id = nil
+    expect(Itemer).to_not be_valid
+  end
+
+  it 'is not valid without a name' do
+    Catagorier.name = nil
+    expect(Catagorier).to_not be_valid
+  end
+
+  it 'is not valid without a amount' do
+    Catagorier.icon = nil
+    expect(Catagorier).to_not be_valid
+  end
+
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  Itemer = Item.new(name: "suger", amount: "4", user_id: 1, catagory_id: 2)
+
+  before { Itemer.save }
+  it 'is valid with valid attributes' do
+    expect(Itemer).to_not be_valid
+  end
+
+  it 'is not valid without an user_id' do
+    Itemer.user_id = nil
+    expect(Itemer).to_not be_valid
+  end
+
+  it 'is not valid without a catagory_id' do
+    Itemer.catagory_id = nil
+    expect(Itemer).to_not be_valid
+  end
+
+  it 'is not valid without a name' do
+    Itemer.name = nil
+    expect(Itemer).to_not be_valid
+  end
+
+  it 'is not valid without a amount' do
+    Itemer.amount = nil
+    expect(Itemer).to_not be_valid
+  end
+
+end
