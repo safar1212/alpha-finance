9241f10f39b020ffe9e66f0e880b01367286718e
safar ali <miqra019@gmail.com>
2022-10-15 16:22:50 +0500

    Add validations for users input in Category and Items section

--- app/models/catagory.rb
+++ app/models/catagory.rb
@@ -1,6 +1,25 @@
 class Catagory < ApplicationRecord
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :icon_type
+
   mount_uploader :icon, IconUploader
 
   belongs_to :user
   has_many :items, dependent: :destroy
+
+  
+
+  private
+
+  def icon_type
+     if icon.attached? == false
+       errors.add(:icon, "is missing!")
+     end
+     if !icon.content_type.in?(%('icon/jpeg icon/png icon/jpg'))
+       errors.add(:icon, "needs to be a jpg, jpeg or png!")
+     end
+   end
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
+
+
+  validates :name, presence: true, length: { maximum: 250 }
+  validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
   belongs_to :user
   belongs_to :catagory
 end
